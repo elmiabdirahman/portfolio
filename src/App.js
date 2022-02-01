f764d05cf3559702ddf0c409b293450d6908655b
Elmi Elmi <elmielmi522@gmail.com>
2022-02-01 17:55:11 -0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Portfolio project</h2>
+    </div>
+  );
+}
+
+export default App;
