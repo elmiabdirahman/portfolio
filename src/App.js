2224668cf945b4fc3dc82c83a723ea8b2836359c
Elmi Elmi <elmielmi522@gmail.com>
2022-02-01 18:12:11 -0600

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css"
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h2>Portfolio project</h2>
+      <Navbar />
     </div>
   );
 }
